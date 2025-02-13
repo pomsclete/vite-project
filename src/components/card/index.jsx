e4795ab60c840fb1ba5513593f3208b40101eb0c
pomsclete <amzo07@yahoo.fr>
2025-02-13 16:41:10 +0000

    Dynamiser les données

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,10 +1,10 @@
 import "./card.css"
-function Card () {
+function Card (props) {
     return (
         <div className="card">
             <a href="" >
-                <img className="card_img" src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg" alt="Placeholder" />
-                <h2 className="card_title">Titre de la card</h2>
+                <img className="card_img" src={props.image} alt="Placeholder" />
+                <h2 className="card_title">{props.title}</h2>
             </a>
         </div>
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,21 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Banner from "../../components/banner/index"
 import Card from "../../components/card/index"
 import "./home.css"
+import DataJson from "../../datas/logements.json"
 function Home () {
+
+    const [data, setData] = useState(null)
 
     useEffect(() => {
         document.title = "Bienvenue à notre site immobilier"
+        setData(DataJson)
     }, []);
 
     return (
         <>
             <Banner />
             <div className='main-content'>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {
+                    data && data.map((item, index) => (
+                        <Card key={index} title={item.title} image={item.cover} />
+                    ))
+                }
             </div>
 
         </>
